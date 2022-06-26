7c71ff43a2489da366ba5d208de5e4680f8806f6
larissacosta98 <larissatcosta98@gmail.com>
2022-06-26 12:40:54 -0300

    A Simple Counter

---
+++ Assets/js/01 - intro.js
@@ -0,0 +1,10 @@
+// var y = 0;
+// funçao 1o grau: ax + = 0
+// ex.: f(1) = a . 1 + b / f(2) = a . 2 + b
+// return mostra e permite que eu manipule o valor, enquanto console.log só mostra
+
+function soma (a,b){ // declara/chama a função
+  return a+b;       // retorna o resultado da função 
+}
+
+soma(3,5);
